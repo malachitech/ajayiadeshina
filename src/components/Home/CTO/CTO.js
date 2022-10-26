f2c6cccb190b4d93b3861c5bbc3fcf27cfabe3e2
xmalachi <angelmalachi@gmail.com>
2022-10-27 00:38:28 +0100

    update

--- src/components/Home/CTO/CTO.js
+++ src/components/Home/CTO/CTO.js
@@ -3,10 +3,12 @@ import Title from '../../Title/Title'
 import styles from '../Home.module.css'
 import {BsChevronDown} from "react-icons/bs";
 import { Link } from 'react-router-dom'
+import img from '../../images/ajayi.jpg'
 
 function CTO() {
   return (
     <div className={styles.cto}>
+        <div className={styles.ctotxt}>
         <div className={styles.entry}>
             <Title text='Hi,' color='goldenrod' />
         </div>
@@ -17,16 +19,14 @@ function CTO() {
                 I am Adeshina Ajayi. I am arguably Africa&apos;s foremost blockchain expert and tech educator.   
                 In the last 6years, I have helped corporate institutions and individuals to understand the relevance of blockchain 
                 technology and positioned them to take their stake in the fast emerging tech space.   I do this through my blockchain 
-                consulting and education firm, Digital Focus.   I have provided blockchain solutions to top institutions from different 
-                sectors like CMC Connect, Alpha Training Lab (ATL), Nerdzfactory, TLCGLOBAL, Truwiters, Addicio Solutions, Code Tutor 
-                to mention a few. My academy, Digital Focus Academy has certified over two thousand (2,000) Blockchain Experts across Nigeria.
+                consulting and education firm, Digital Focus. 
             </p>
         </div>
 
         <div className={styles.ctobtn}>
             <div className={styles.more}>
                 <Link to='/about'>
-                    More
+                    Read More
                 </Link>
             </div>
 
@@ -39,6 +39,11 @@ function CTO() {
 
         <div className={styles.toBottom}>
             <BsChevronDown />
+        </div>
+        </div>
+
+        <div className={styles.ctoimg}>
+            <img src={img} alt="standing and facing the side" />
         </div>
     </div>
   )
