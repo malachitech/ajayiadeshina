bf06fd83a5ec331c139a772760bae5f2594ab5bb
xmalachi <angelmalachi@gmail.com>
2023-06-02 11:18:12 +0100

    update

--- src/components/Screen/UberScreen/Listing.js
+++ src/components/Screen/UberScreen/Listing.js
@@ -4,73 +4,6 @@ import {MdOutlineKeyboardArrowRight}  from 'react-icons/md'
 function Listing() {
     
         const list = [
-            // {
-            //     id: 8,
-            //     title: '10 Alimosho Rd',
-            //     date: 'May 25·5:25AM',
-            //     price: '4000.00'
-            // },
-            // {
-            //     id: 7,
-            //     title: 'Ikoyi 106104',
-            //     date: 'May 25·5:33PM',
-            //     price: '7500.00'
-            // },
-            // {
-            //     id: 6,
-            //     title: '10 Alimosho Rd',
-            //     date: 'May 24·9:30AM',
-            //     price: '8000.00'
-            // },
-            // {
-            //     id: 5,
-            //     title: 'Ikoyi 106104',
-            //     date: 'May 24·5:20PM',
-            //     price: '6000.00'
-            // },
-            // {
-            //     id: 4,
-            //     title: '10 Alimosho Rd',
-            //     date: 'May 23·9:10AM',
-            //     price: '7500.00'
-            // },
-            // {
-            //     id: 3,
-            //     title: 'Ikoyi 106104',
-            //     date: 'May 23·5:41PM',
-            //     price: '6500.00'
-            // },
-            
-            // {
-            //     id: 2,
-            //     title: '10 Alimosho Rd',
-            //     date: 'May 22·8:34AM',
-            //     price: '7500.00'
-            // },
-            // {
-            //     id: 1,
-            //     title: 'Ikoyi 106104',
-            //     date: 'May 22·5:30PM',
-            //     price: '7000.00'
-            // },
-            {
-                id: 10,
-                title: '12 Gloryfield Avenue',
-                date: 'May 30·5:10PM',
-                price: '9700.00'
-            },
-            {
-                id: 9,
-                title: '24b Eric Moore Street ',
-                date: 'May 30·9:00AM',
-                price: '4200.00'
-            },
-            {
-                id: 11,
-                title: '33b Mobolaji Johnson Ave',
-                date: 'May 26·4:44PM',
-                price: '0.00 · Canceled' 
-            },
             {
                 id: 8,
                 title: '10 Alimosho Rd',
@@ -78,29 +11,96 @@ function Listing() {
                 price: '4000.00'
             },
             {
-                    id: 7,
-                    title: 'Ikoyi 106104',
-                    date: 'May 25·5:33AM',
-                    price: '7500.00'
-                },
-                {
-                    id: 6,
-                    title: '10 Alimosho Rd',
-                    date: 'May 24·9:30PM',
-                    price: '8000.00'
-                },
-                {
-                    id: 5,
-                    title: 'Ikoyi 106104',
-                    date: 'May 24·5:20AM',
-                    price: '6000.00'
-                },
-                {
-                    id: 4,
-                    title: '10 Alimosho Rd',
-                    date: 'May 23·9:10PM',
-                    price: '7500.00'
-                },
+                id: 7,
+                title: 'Ikoyi 106104',
+                date: 'May 25·5:33AM',
+                price: '7500.00'
+            },
+            {
+                id: 6,
+                title: '10 Alimosho Rd',
+                date: 'May 24·9:30PM',
+                price: '8000.00'
+            },
+            {
+                id: 5,
+                title: 'Ikoyi 106104',
+                date: 'May 24·5:20AM',
+                price: '6000.00'
+            },
+            {
+                id: 4,
+                title: '10 Alimosho Rd',
+                date: 'May 23·9:10PM',
+                price: '7500.00'
+            },
+            {
+                id: 3,
+                title: 'Ikoyi 106104',
+                date: 'May 23·5:41AM',
+                price: '6500.00'
+            },
+            
+            {
+                id: 2,
+                title: '10 Alimosho Rd',
+                date: 'May 22·8:34PM',
+                price: '7500.00'
+            },
+            {
+                id: 1,
+                title: 'Ikoyi 106104',
+                date: 'May 22·5:30AM',
+                price: '7000.00'
+            },
+            // {
+            //     id: 10,
+            //     title: '12 Gloryfield Avenue',
+            //     date: 'May 30·5:10PM',
+            //     price: '9700.00'
+            // },
+            // {
+            //     id: 9,
+            //     title: '24b Eric Moore Street ',
+            //     date: 'May 30·9:00AM',
+            //     price: '4200.00'
+            // },
+            // {
+            //     id: 11,
+            //     title: '33b Mobolaji Johnson Ave',
+            //     date: 'May 26·4:44PM',
+            //     price: '0.00 · Canceled' 
+            // },
+            // {
+            //     id: 8,
+            //     title: '10 Alimosho Rd',
+            //     date: 'May 25·5:25PM',
+            //     price: '4000.00'
+            // },
+            // {
+            //         id: 7,
+            //         title: 'Ikoyi 106104',
+            //         date: 'May 25·5:33AM',
+            //         price: '7500.00'
+            //     },
+            //     {
+            //         id: 6,
+            //         title: '10 Alimosho Rd',
+            //         date: 'May 24·9:30PM',
+            //         price: '8000.00'
+            //     },
+            //     {
+            //         id: 5,
+            //         title: 'Ikoyi 106104',
+            //         date: 'May 24·5:20AM',
+            //         price: '6000.00'
+            //     },
+            //     {
+            //         id: 4,
+            //         title: '10 Alimosho Rd',
+            //         date: 'May 23·9:10PM',
+            //         price: '7500.00'
+            //     },
             
         ]
   return (
